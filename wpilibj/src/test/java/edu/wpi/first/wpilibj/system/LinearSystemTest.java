4ab736ca84bb42714ba72af0f1ac4986070f5320
Matt <matthew.morley.ca@gmail.com>
2020-04-01 17:10:46 -0700

    add linearsystem tests

--- wpilibj/src/test/java/edu/wpi/first/wpilibj/system/LinearSystemTest.java
+++ wpilibj/src/test/java/edu/wpi/first/wpilibj/system/LinearSystemTest.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpiutil.math.MatBuilder;
 import edu.wpi.first.wpiutil.math.Nat;
 import org.junit.jupiter.api.Test;
 
+
 import static org.ejml.EjmlUnitTests.assertEquals;
 
 class LinearSystemTest {
@@ -24,6 +25,40 @@ class LinearSystemTest {
 
     assertEquals(model.getD().getStorage().getDDRM(), new MatBuilder<>(Nat.N2(),
             Nat.N2()).fill(0.0, 0.0, 0.0, 0.0).getStorage().getDDRM(), 0.001);
+  }
+
+  @Test
+  public void testElevatorSystem() {
+
+    var model = LinearSystem.createElevatorSystem(DCMotor.getNEO(2), 5, 0.05, 12, 12);
+    assertEquals(model.getA().getStorage().getDDRM(), new MatBuilder<>(Nat.N2(),
+            Nat.N2()).fill(0, 1, 0, -99.05473).getStorage().getDDRM(), 0.001);
+
+    assertEquals(model.getB().getStorage().getDDRM(), new MatBuilder<>(Nat.N2(),
+            Nat.N1()).fill(0, 20.8).getStorage().getDDRM(), 0.001);
+
+    assertEquals(model.getC().getStorage().getDDRM(), new MatBuilder<>(Nat.N1(),
+            Nat.N2()).fill(1, 0).getStorage().getDDRM(), 0.001);
+
+    assertEquals(model.getD().getStorage().getDDRM(), new MatBuilder<>(Nat.N1(),
+            Nat.N1()).fill(0).getStorage().getDDRM(), 0.001);
+  }
+
+  @Test
+  public void testFlywheelSystem() {
+
+    var model = LinearSystem.createFlywheelSystem(DCMotor.getNEO(2), 0.00032, 1.0,  12);
+    assertEquals(model.getA().getStorage().getDDRM(), new MatBuilder<>(Nat.N1(),
+            Nat.N1()).fill(-26.87032).getStorage().getDDRM(), 0.001);
+
+    assertEquals(model.getB().getStorage().getDDRM(), new MatBuilder<>(Nat.N1(),
+            Nat.N1()).fill(1354.166667).getStorage().getDDRM(), 0.001);
+
+    assertEquals(model.getC().getStorage().getDDRM(), new MatBuilder<>(Nat.N1(),
+            Nat.N1()).fill(1).getStorage().getDDRM(), 0.001);
+
+    assertEquals(model.getD().getStorage().getDDRM(), new MatBuilder<>(Nat.N1(),
+            Nat.N1()).fill(0).getStorage().getDDRM(), 0.001);
   }
 
   @Test
